b3dc5fba79a6e9aa1195ab99e32e1030de3c3911
Walik23 <122877885+Walik23@users.noreply.github.com>
2024-05-24 15:17:01 +0300

    Update op.go

--- painter/op.go
+++ painter/op.go
@@ -1,48 +1,104 @@
 package painter
 
 import (
+	"image"
 	"image/color"
+
+	"github.com/roman-mazur/architecture-lab-3/ui"
 
 	"golang.org/x/exp/shiny/screen"
 )
 
-// Operation змінює вхідну текстуру.
 type Operation interface {
-	// Do виконує зміну операції, повертаючи true, якщо текстура вважається готовою для відображення.
-	Do(t screen.Texture) (ready bool)
+	Update(state *TextureState)
 }
 
-// OperationList групує список операції в одну.
+type TextureOperation interface {
+	Do(t screen.Texture)
+	Update(state *TextureState)
+}
+
 type OperationList []Operation
 
-func (ol OperationList) Do(t screen.Texture) (ready bool) {
-	for _, o := range ol {
-		ready = o.Do(t) || ready
+var UpdateOp = Update{}
+
+type Update struct{}
+
+func (op Update) Update(_ *TextureState) {}
+
+type Fill struct {
+	Color color.Color
+}
+
+func (op Fill) Do(t screen.Texture) {
+	t.Fill(t.Bounds(), op.Color, screen.Src)
+}
+
+func (op Fill) Update(state *TextureState) {
+	state.backgroundColor = &op
+}
+
+type Reset struct{}
+
+var ResetOp = Reset{}
+
+func (op Reset) Update(state *TextureState) {
+	state.backgroundColor = &Fill{Color: color.Black}
+	state.backgroundRect = nil
+	state.figureCenters = nil
+}
+
+type BgRect struct {
+	X1 float32
+	Y1 float32
+	X2 float32
+	Y2 float32
+}
+
+func (op BgRect) Do(t screen.Texture) {
+	t.Fill(
+		image.Rect(
+			int(op.X1*float32(t.Size().X)),
+			int(op.Y1*float32(t.Size().Y)),
+			int(op.X2*float32(t.Size().X)),
+			int(op.Y2*float32(t.Size().Y)),
+		),
+		color.Black,
+		screen.Src,
+	)
+}
+
+func (op BgRect) Update(state *TextureState) {
+	state.backgroundRect = &op
+}
+
+type Figure struct {
+	X float32
+	Y float32
+}
+
+func (op Figure) Do(t screen.Texture) {
+	ui.DrawCross(
+		t,
+		image.Pt(
+			int(op.X*float32(t.Size().X)),
+			int(op.Y*float32(t.Size().Y)),
+		),
+	)
+}
+
+func (op Figure) Update(state *TextureState) {
+	state.figureCenters = append(state.figureCenters, &op)
+}
+
+type Move struct {
+	X float32
+	Y float32
+}
+
+func (op Move) Update(state *TextureState) {
+	for _, fig := range state.figureCenters {
+		fig.X = op.X
+		fig.Y = op.Y
 	}
-	return
-}
-
-// UpdateOp операція, яка не змінює текстуру, але сигналізує, що текстуру потрібно розглядати як готову.
-var UpdateOp = updateOp{}
-
-type updateOp struct{}
-
-func (op updateOp) Do(t screen.Texture) bool { return true }
-
-// OperationFunc використовується для перетворення функції оновлення текстури в Operation.
-type OperationFunc func(t screen.Texture)
-
-func (f OperationFunc) Do(t screen.Texture) bool {
-	f(t)
-	return false
-}
-
-// WhiteFill зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
-func WhiteFill(t screen.Texture) {
-	t.Fill(t.Bounds(), color.White, screen.Src)
-}
-
-// GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
-func GreenFill(t screen.Texture) {
-	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
 }
