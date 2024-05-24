bedb6ef051314e8a0eeb6e1adf819f2a9966a55f
Walik23 <122877885+Walik23@users.noreply.github.com>
2024-05-24 15:16:38 +0300

    Update loop.go

--- painter/loop.go
+++ painter/loop.go
@@ -2,59 +2,77 @@ package painter
 
 import (
 	"image"
+	"image/color"
 
 	"golang.org/x/exp/shiny/screen"
 )
 
-// Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циклі подій.
 type Receiver interface {
 	Update(t screen.Texture)
 }
 
-// Loop реалізує цикл подій для формування текстури отриманої через виконання операцій отриманих з внутрішньої черги.
 type Loop struct {
 	Receiver Receiver
 
-	next screen.Texture // текстура, яка зараз формується
-	prev screen.Texture // текстура, яка була відправлення останнього разу у Receiver
+	next screen.Texture
+	prev screen.Texture
 
-	mq messageQueue
-
-	stop    chan struct{}
-	stopReq bool
+	mq       MessageQueue
+	state    TextureState
+	doneFunc func()
 }
 
-var size = image.Pt(400, 400)
+var size = image.Pt(600, 600)
 
-// Start запускає цикл подій. Цей метод потрібно запустити до того, як викликати на ньому будь-які інші методи.
 func (l *Loop) Start(s screen.Screen) {
 	l.next, _ = s.NewTexture(size)
-	l.prev, _ = s.NewTexture(size)
+	l.mq = MessageQueue{queue: make(chan Operation)}
+	l.state = TextureState{backgroundColor: &Fill{Color: color.White}}
 
-	// TODO: стартувати цикл подій.
+	go func() {
+		for {
+			e := l.mq.Pull()
+
+			switch e.(type) {
+			case Figure, BgRect, Move, Fill, Reset:
+				e.Update(&l.state)
+			case Update:
+				l.state.backgroundColor.Do(l.next)
+
+				if l.state.backgroundRect != nil {
+					l.state.backgroundRect.Do(l.next)
+				}
+
+				for _, fig := range l.state.figureCenters {
+					fig.Do(l.next)
+				}
+				l.prev = l.next
+				l.Receiver.Update(l.next)
+				l.next, _ = s.NewTexture(size)
+			}
+
+			if l.doneFunc != nil {
+				l.doneFunc()
+			}
+		}
+	}()
 }
 
-// Post додає нову операцію у внутрішню чергу.
-func (l *Loop) Post(op Operation) {
-	if update := op.Do(l.next); update {
-		l.Receiver.Update(l.next)
-		l.next, l.prev = l.prev, l.next
+func (l *Loop) Post(ol OperationList) {
+
+	for _, op := range ol {
+		l.mq.Push(op)
 	}
 }
 
-// StopAndWait сигналізує про необхідність завершити цикл та блокується до моменту його повної зупинки.
-func (l *Loop) StopAndWait() {
+type MessageQueue struct {
+	queue chan Operation
 }
 
-// TODO: Реалізувати чергу подій.
-type messageQueue struct{}
-
-func (mq *messageQueue) push(op Operation) {}
-
-func (mq *messageQueue) pull() Operation {
-	return nil
+func (mq *MessageQueue) Push(op Operation) {
+	mq.queue <- op
 }
 
-func (mq *messageQueue) empty() bool {
-	return false
+func (mq *MessageQueue) Pull() Operation {
+	return <-mq.queue
 }
